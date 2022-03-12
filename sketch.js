aa31bdf50edff5c1ab4102f211ccfefbc3321e20
sumit-806 <96688309+sumit-806@users.noreply.github.com>
2022-03-12 20:09:40 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -41,47 +41,47 @@ function draw() {
 
 // var select_sprites = Math.random(random(1,3));
 
-var select_sprites = Math.round(1,3);
+//var select_sprites = Math.round(1,3);
 
-// var select_sprites = Math.round(random(1,3));
+ var select_sprites = Math.round(random(1,3));
 
   
-  // if (frameCount % 50 == 0) {
-   //  if (select_sprites == 1) {
-   //   createApples();
-  // } else if (select_sprites == 2) {
-  //    createOrange();
-  //  }else {
-  //    createRed();
-   //  }
-  // }
-
-   if (frameCount % 80 == 0) {
-    if (select_sprites == 1) {
-       createApples();
-    } else if (select_sprites == 2) {
+   if (frameCount % 50 == 0) {
+     if (select_sprites == 1) {
+      createApples();
+   } else if (select_sprites == 2) {
       createOrange();
+   }else {
+      createRed();
      }
-   }
+  }
 
-  // if (frameCount / 80 == 0) {
-  //   if (select_sprites == 1) {
-  //     createApples();
-  //   } else if (select_sprites == 2) {
-  //     createOrange();
-  //   }else {
-  //     createRed();
-  //   }
+  // if (frameCount % 80 == 0) {
+ // if (select_sprites == 1) {
+   //    createApples();
+   // } else if (select_sprites == 2) {
+   // createOrange();
   // }
+   //}
+//
+  //if (frameCount / 80 == 0) {
+  //  if (select_sprites == 1) {
+   //  createApples();
+    // } else if (select_sprites == 2) {
+   //    createOrange();
+  //  }else {
+ ///    createRed();
+  // }
+// }
 
-  // if (frameCount % 80 = 0) {
-  //   if (select_sprites == 1) {
-  //     createApples();
-  //   } else if (select_sprites == 2) {
-  //     createOrange();
-  //   }else {
-  //     createRed();
-  //   }
+  //if (frameCount % 80 = 0) {
+   // if (select_sprites == 1) {
+     //  createApples();
+     //} else if (select_sprites == 2) {
+     //  createOrange();
+    // }else {
+   //    createRed();
+   //  }
   // }
 
 
